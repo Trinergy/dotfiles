29e8e6955e15106aa1b5d56141f0b04c4823a0c1
Kenny Wu <wu.kenny1221@gmail.com>
2017-02-09 15:16:03 -0800

    add hammerspoon

---
+++ hammerspoon/init.lua
@@ -0,0 +1,57 @@
+-- Application Hotkey
+alpha = {"cmd", "ctrl"}
+-- Windows Hotkey
+beta = {"cmd", "shift"}
+
+hs.hotkey.bind(alpha, "N", function()
+    hs.application.open("Evernote")
+end)
+
+hs.hotkey.bind(alpha, "C", function()
+    hs.application.open("Google Chrome")
+end)
+
+hs.hotkey.bind(alpha, "E", function()
+    hs.application.open("Emacs")
+end)
+
+hs.hotkey.bind(alpha, "L", function()
+    hs.application.open("LINE")
+end)
+
+hs.hotkey.bind(alpha, "S", function()
+    hs.application.open("Slack")
+end)
+
+hs.hotkey.bind(alpha, "A", function()
+    hs.application.open("Anki")
+end)
+
+hs.hotkey.bind(alpha, "F", function()
+    hs.application.open("Finder")
+end)
+
+hs.hotkey.bind(alpha, "M", function()
+    hs.application.open("Spotify")
+end)
+
+hs.hotkey.bind(alpha, "G", function()
+    hs.application.open("GitHub Desktop")
+end)
+
+hs.hotkey.bind(alpha, "D", function()
+    hs.application.open("Mail")
+end)
+
+hs.hotkey.bind(alpha, "W", function()
+    hs.application.open("Terminal")
+end)
+
+hs.hotkey.bind(alpha, "P", function()
+    hs.application.open("Preview")
+end)
+
+-- hs.hotkey.bind(beta, "O", function()
+--     local w = hs.window.focusedWindow()
+--     w:maximize()
+-- end)
